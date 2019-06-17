4b0edc95820d2c07798bd01d33f2fad5fb9270ea
T3 Alamri <t3@t3s-macbook-pro.wireless.rit.edu>
2019-06-17 14:24:43 -0400

    adding docker-compose update and update python scripts

--- mainAPID/mainAPI.py
+++ mainAPID/mainAPI.py
@@ -1,25 +1,19 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
-from sqlalchemy import create_engine
-from json import dumps
-from flask.ext.jsonpify import jsonify
+#from flask_restful import Resource, Api
+#from sqlalchemy import create_engine
+#from json import dumps
+#from flask.ext.jsonpify import jsonify
 
-
-db = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
-api = Api(app)
-
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Hello, this is mainAPI"
 
 
 
 def main():
     app.run(debug=True, host="0.0.0.0", port=5000)
-
-
 
 
 if __name__ == '__main__':
