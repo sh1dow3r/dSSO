a3019668d36a671951ab0ac50535dad0a1c27a17
sh1dow3r <sh1dow3r@gmail.com>
2019-06-16 20:51:11 -0400

    adding dockerfiles + orgnize structre

--- mainAPID/mainAPI.py
+++ mainAPID/mainAPI.py
@@ -1,1 +1,28 @@
-print("hello this mainAPI")
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask.ext.jsonpify import jsonify
+
+
+db = create_engine('sqlite:///chinook.db')
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
