4971d9e1bdf000a449edc50b28c3d1c688be8a73
sh1dow3r <sh1dow3r@gmail.com>
2019-06-17 14:24:43 -0400

    adding docker-compose update and update python scripts

--- serverD/server.py
+++ serverD/server.py
@@ -1,1 +1,22 @@
-print("hello this is server")
+from flask import Flask, request
+#from flask_restful import Resource, Api
+#from sqlalchemy import create_engine
+#from json import dumps
+#from flask.ext.jsonpify import jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, this is server"
+
+
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
+
+if __name__ == '__main__':
+    main()
+
+
